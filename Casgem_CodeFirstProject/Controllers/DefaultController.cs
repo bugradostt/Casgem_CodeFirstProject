c3204e40525e49e3618c3983ae1c5c136e8e53ce
budotekno <budotekno@gmail.com>
2023-07-15 10:06:32 +0300

    add admin

--- Casgem_CodeFirstProject/Controllers/DefaultController.cs
+++ Casgem_CodeFirstProject/Controllers/DefaultController.cs
@@ -1,17 +1,57 @@
-﻿using System;
+﻿using Casgem_CodeFirstProject.Dal.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace Casgem_CodeFirstProject.Controllers
 {
     public class DefaultController : Controller
     {
+
+        TravelContext c = new TravelContext();
         // GET: Default
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult _PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _PartialSlider()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _PartialBooking()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _PartialMoveTop()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _PartialDestinations()
+        {
+            var values = c.Destinations.ToList();
+            return PartialView(values);
         }
     }
 }
